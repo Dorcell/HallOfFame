53869a43b7ad77e42ae4649ab190be0919f09ec5
Dorcell <death_page@mail.ru>
2019-04-08 17:12:59 +0700

    How EF works

--- TestApp/TestApp/Controllers/HomeController.cs
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestApp.Models;
 
 namespace TestApp.Controllers
 {
     public class HomeController : Controller
     {
+        private EFDBContext _context;
+
+        public HomeController(EFDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             HomeModel _model = new HomeModel() { HelloMessage = "Hello" };
-            return View(_model);
+            List<Directory> _dirs = _context.Directory.Include(x=>x.Materials).ToList();
+            return View(_dirs);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
