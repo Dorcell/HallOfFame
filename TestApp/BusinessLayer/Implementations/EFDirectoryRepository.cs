7b4fb4f1e6a739916ce7d99593991aa06437b6ea
Dorcell <death_page@mail.ru>
2019-04-08 18:01:40 +0700

    directories interface and realisation

---
+++ TestApp/BusinessLayer/Implementations/EFDirectoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Interfaces;
+using DataLayer;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer.Implementations
+{
+    class EFDirectoryRepository : IDirectoryRepository
+    {
+        private EFDBContext context;
+
+        public EFDirectoryRepository(EFDBContext context)
+        {
+            this.context = context;
+        }        
+
+        public IEnumerable<Directory> GetAllDirectories(bool includeMaterials = false)
+        {
+            if (includeMaterials)
+                return context.Set<Directory>().Include(x => x.Materials).AsNoTracking().ToList();
+            else
+                return context.Directory.ToList();
+            
+            //throw new NotImplementedException();
+        }
+
+        public Directory GetDirectoryById(int directoryId, bool IncludeMaterials = false)
+        {
+            if (IncludeMaterials)
+                return context.Set<Directory>().Include(x => x.Materials).AsNoTracking().FirstOrDefault(x => x.Id == directoryId);
+            else
+                return context.Directory.FirstOrDefault(x => x.Id == directoryId);
+            
+            //throw new NotImplementedException();
+        }
+
+        public void SaveDirectory(Directory achieve)
+        {
+            if (achieve.Id == 0)
+                context.Directory.Add(achieve);
+            else
+                context.Entry(achieve).State = EntityState.Modified;
+            
+            //throw new NotImplementedException();
+        }
+
+        public void DeleteDirectory(Directory achieve)
+        {
+            context.Directory.Remove(achieve);
+            context.SaveChanges();
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestApp/BusinessLayer/Interfaces/IDirectoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IDirectoryRepository
+    {
+        IEnumerable<Directory> GetAllDirectories(bool includeMaterials = false);
+
+        Directory GetDirectoryById(int directoryId, bool IncludeMaterials = false);
+
+        void SaveDirectory(Directory achieve);
+
+        void DeleteDirectory(Directory achieve);
+    }
+}
